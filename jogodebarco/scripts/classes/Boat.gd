4dec5bbf1e317b9a049d6b502b64463c7a3bc99f
hugui <joaohugo.prates@gmail.com>
2026-02-17 18:13:29 -0800

    mov: melhora e centralização do movimento no Boat, inclusão do movement node no Boat

--- jogodebarco/scripts/classes/Boat.gd
+++ jogodebarco/scripts/classes/Boat.gd
@@ -1,15 +1,33 @@
 extends CharacterBody2D
-
 class_name Boat
 
-var crew : int
-var free_crew : int
+@onready var movement: Movement = $Movement
+
+var crew := 0
+var free_crew := 0
 
 signal crew_reset
 
-func _ready() -> void:
-	connect('crew_reset', reset_crew)
+func _ready():
+	connect("crew_reset", reset_crew)
+
+func _physics_process(delta):
+	_handle_movement(delta)
 
 func reset_crew():
 	free_crew = crew
-	print('asdf')
+	movement.reset_crew()
+
+func _handle_movement(delta):
+
+	if movement.anchored:
+		movement.speed -= movement.deaccel * delta
+	else:
+		movement.speed += movement.accel * delta
+
+	movement.speed = clamp(movement.speed, 0, movement.max_speed)
+
+	var forward = Vector2.UP.rotated(rotation)
+	velocity = forward * movement.speed
+
+	move_and_slide()
